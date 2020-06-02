a82005a4ac3a9ad16079521ea40a2d761be613fb
andy-aguilar <aaguil3@gmail.com>
2020-06-02 16:39:02 -0400

    some changes after following Paul's coding

--- newsletter.rb
+++ newsletter.rb
@@ -9,28 +9,43 @@ CAMPUS = {
 }
 DATE = "Nov 20, 2019"
 
-SUBSCRIBERS = ["rhona@grimes.info", "cedricschmidt@robel.io", "edmond@ko.org", "bryant@cummingsfisher.biz", "alverta@bernhard.name"]
+SUBSCRIBERS = ["rhona@grimes.info", "cedricschmidt@robel.io", "edmond@ko.org", 
+"bryant@cummingsfisher.biz", "alverta@bernhard.name"]
 UNSUBSCRIBED = ["cedricschmidt@robel.io", "alverta@bernhard.name"]
 
 ARTICLES = [
-  {author: "Destiny Blanda Bruen II", title: "Mining", text: "The orthogonal features, when combined, can explode into complexity."},
-  {author: "Robin Flatley Hilpert", title: "Retail", text: "In our daily lives as programmers, we process text strings a lot. So I tried to work hard on text processing, namely the string class and regular expressions. Regular expressions are built into the language and are very tuned up for use."},
-  {author: "Olevia Torphy Kuvalis", title: "Technology", text: "Imagine you are writing an email. You are in front of the computer. You are operating the computer, clicking a mouse and typing on a keyboard, but the message will be sent to a human over the internet. So you are working before the computer, but with a human behind the computer."},
-  {author: "Dr. Crystle Kovacek Denesik", title: "Legal", text: "Most programs are not write-once. They are reworked and rewritten again and again in their lived. Bugs must be debugged. Changing requirements and the need for increased functionality mean the program itself may be modified on an ongoing basis. During this process, human beings must be able to read and understand the original code. It is therefore more important by far for humans to be able to understand the program than it is for the computer."},
-  {author: "Alfred Jast Hermann", title: "Real-Estate", text: "I didn't work hard to make Ruby perfect for everyone, because you feel differently from me. No language can be perfect for everyone. I tried to make Ruby perfect for me, but maybe it's not perfect for you. The perfect language for Guido van Rossum is probably Python."},
-  {author: "Michale Bruen Boehm", title: "Consulting", text: "Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so therefore Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only."},
-  {author: "Tony Keeling Cartwright", title: "Design", text: "Often people, especially computer engineers, focus on the machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines."},
+  {author: "Destiny Blanda Bruen II", 
+  title: "Mining", 
+  text: "The orthogonal features, when combined, can explode into complexity."},
+  {author: "Robin Flatley Hilpert", 
+  title: "Retail", 
+  text: "In our daily lives as programmers, we process text strings a lot. So I tried to work hard on text processing, namely the string class and regular expressions. Regular expressions are built into the language and are very tuned up for use."},
+  {author: "Olevia Torphy Kuvalis", 
+  title: "Technology", 
+  text: "Imagine you are writing an email. You are in front of the computer. You are operating the computer, clicking a mouse and typing on a keyboard, but the message will be sent to a human over the internet. So you are working before the computer, but with a human behind the computer."},
+  {author: "Dr. Crystle Kovacek Denesik", 
+  title: "Legal", 
+  text: "Most programs are not write-once. They are reworked and rewritten again and again in their lived. Bugs must be debugged. Changing requirements and the need for increased functionality mean the program itself may be modified on an ongoing basis. During this process, human beings must be able to read and understand the original code. It is therefore more important by far for humans to be able to understand the program than it is for the computer."},
+  {author: "Alfred Jast Hermann", 
+  title: "Real-Estate", 
+  text: "I didn't work hard to make Ruby perfect for everyone, because you feel differently from me. No language can be perfect for everyone. I tried to make Ruby perfect for me, but maybe it's not perfect for you. The perfect language for Guido van Rossum is probably Python."},
+  {author: "Michale Bruen Boehm",
+  title: "Consulting", 
+  text: "Everyone has an individual background. Someone may come from Python, someone else may come from Perl, and they may be surprised by different aspects of the language. Then they come up to me and say, 'I was surprised by this feature of the language, so therefore Ruby violates the principle of least surprise.' Wait. Wait. The principle of least surprise is not for you only."},
+  {author: "Tony Keeling Cartwright", 
+  title: "Design", 
+  text: "Often people, especially computer engineers, focus on the machines. But in fact we need to focus on humans, on how humans care about doing programming or operating the application of the machines."},
 ]
 
 #########################
 # Methods to generate the newsletter
 #########################
 
-def calculate_recipients (subs, unsubs)
+def calculate_recipients
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # Returns an array of only the subscribers who haven't unsubscribed
- subs.find_all do |unsubscribed_readers|
-    !unsubs.include? unsubscribed_readers
+ SUBSCRIBERS.find_all do |unsubscribed_readers|
+    !UNSUBSCRIBED.include? unsubscribed_readers
   end
 end
 
@@ -42,7 +57,7 @@ end
 def print_recipients
   # Returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
-  print "#{calculate_recipients(SUBSCRIBERS, UNSUBSCRIBED).join( ", ")}\n"
+  print "#{calculate_recipients.join( ", ")}\n"
 end
 
 def print_one_article(article)
@@ -51,7 +66,7 @@ def print_one_article(article)
 end
 
 def print_many_articles(articles)
-  # Takes in an array of article hashes (ex. from first_n_articles) and format each one using the print_one_article method
+  # Takes in an array of article hashes (ex. from first_n_articles) and formats each one using the print_one_article method
   articles.each do |article_hash|
     print_one_article(article_hash)
   end
